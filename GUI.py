02bcecbd8cc37a58e7de2a715a738d26de58f0da
Mike <medfordm16@gmail.com>
2021-04-13 02:45:49 -0500

    Added dark theme and pause button

--- GUI.py
+++ GUI.py
@@ -1,51 +1,55 @@
 ﻿from tkinter import *
+from tkinter import ttk
 import time 
 
 
 root = Tk()
-root.geometry("300x250")
-root.title("Time Counter")
+root.geometry("250x265")
+root.title("Rosh Timer")
+root.configure(bg="#23272A")
+
 frame = Frame(root)
-
-
-paused = 0
-separator = "\n----------------"
+frame.configure(bg="#23272A")
 
 aegisTime = 300
-aegisLabel = Label(frame, text="\nAegis"+separator)
-aegisTimer = Label(frame, text="")
-
 roshMinTime = 480
-roshMinLabel = Label(frame, text="\nRosh Min Spawn"+separator)
-roshMinTimer = Label(frame, text="")
-
 roshMaxTime = 660
-roshMaxLabel = Label(frame, text="\nRosh Max Spawn"+separator)
-roshMaxTimer = Label(frame, text="")
 
-resetButton = Button(frame, text = "Reset")
+aegisLabel = Label(frame, text="\nAegis:\n", bg="#23272A", fg="#FFFFFF")
+roshMinLabel = Label(frame, text="\nRosh Min Spawn:\n", bg="#23272A", fg="#FFFFFF")
+roshMaxLabel = Label(frame, text="\nRosh Max Spawn:\n", bg="#23272A", fg="#FFFFFF")
+
+aegisTimer = Label(frame, text="", bg="#23272A", fg="#FFFFFF")
+roshMinTimer = Label(frame, text="", bg="#23272A", fg="#FFFFFF")
+roshMaxTimer = Label(frame, text="", bg="#23272A", fg="#FFFFFF")
+
+sep = ttk.Separator(frame, orient='horizontal')
+sep1 = ttk.Separator(frame, orient='horizontal')
+sep2 = ttk.Separator(frame, orient='horizontal')
 
 frame.pack()
 
 aegisLabel.pack()
+sep.place(relwidth=50, y=33)
 aegisTimer.pack()
 
 roshMinLabel.pack()
+sep1.place(relwidth=50, y=105)
 roshMinTimer.pack()
 
 roshMaxLabel.pack()
+sep2.place(relwidth=50, y=177)
 roshMaxTimer.pack()
 
-resetButton.pack()
 
 def countdown(aegisText, minText, maxText):
     
     global aegisTime
     global roshMinTime
     global roshMaxTime
+    global paused
 
-    while not paused:
-        
+    while paused == 0: 
         if aegisTime > -1:
             aegisText.configure(text = formatTime(aegisTime))
             aegisTime -= 1
@@ -55,25 +59,50 @@ def countdown(aegisText, minText, maxText):
             roshMinTime -= 1
                    
         if roshMaxTime > -1:
-            maxText.configure(text = formatTime(roshMaxTime))
+            maxText.configure(text = formatTime(roshMaxTime)+'\n')
             roshMaxTime -= 1
-
         time.sleep(1)
         root.update()
-        
+     
 def reset():
+    global aegisTime
+    global roshMinTime
+    global roshMaxTime
 
     aegisTime = 300
     roshMinTime = 480
     roshMaxTime = 660
-
     countdown(aegisTimer, roshMinTimer, roshMaxTimer)
-
 
 def formatTime(seconds):
     mins, secs = divmod(seconds, 60)
     return '{:02d}:{:02d}'.format(mins, secs)
 
-resetButton.configure(command = reset())
+def pauseTimer():
+    global paused
+    global aegisTime
+    global roshMinTime
+    global roshMaxTime
+
+    if paused == 1:
+        paused -= 1
+        pauseLabel = "Pause"
+    else:
+        paused += 1
+        pauseLabel = "Resume"
+
+    pauseButton.configure(text = pauseLabel)
+    countdown(aegisTimer, roshMinTimer, roshMaxTimer)
+
+
+paused = 0
+pauseLabel = "Pause"
+pauseButton = Button(frame, text = pauseLabel, command = pauseTimer, bg="#23272A", fg="#FFFFFF")
+pauseButton.pack(side = LEFT )
+
+resetButton = Button(frame, text = "Reset", command = reset, bg="#23272A", fg="#FFFFFF")
+resetButton.pack(side = RIGHT)
 countdown(aegisTimer, roshMinTimer, roshMaxTimer)
+
 root.mainloop()
+
